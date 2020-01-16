0f12437abd2264c22bfc76f99dbdeb6d6bcd0531
Mark Diekhans <markd@ucsc.edu>
2020-01-16 11:13:25 -0800

    Revert "64bit blat work"
    This reverts commit 13f9e552cf168647c7044e339108f2ece5151ad5.

--- src/jkOwnLib/genoFind.c
+++ src/jkOwnLib/genoFind.c
@@ -1205,6 +1205,7 @@ static void findClumpBounds(struct gfClump *clump, int tileSize)
 /* Figure out qStart/qEnd tStart/tEnd from hitList */
 {
 struct gfHit *hit;
+int x;
 hit = clump->hitList;
 if (hit == NULL)
     return;
@@ -1212,7 +1213,7 @@ clump->qStart = clump->qEnd = hit->qStart;
 clump->tStart = clump->tEnd = hit->tStart;
 for (hit = hit->next; hit != NULL; hit = hit->next)
     {
-    gfOffset x = hit->qStart;
+    x = hit->qStart;
     if (x < clump->qStart) clump->qStart = x;
     if (x > clump->qEnd) clump->qEnd = x;
     x = hit->tStart;
@@ -1447,8 +1448,8 @@ int tileSizeMinusOne = gf->tileSize - 1;
 int mask = gf->tileMask;
 DNA *dna = seq->dna;
 int i, j;
-bits32 bits = 0;
-bits32 bVal;
+gfOffset bits = 0;
+gfOffset bVal;
 int listSize;
 gfOffset qStart, *tList;
 int hitCount = 0;
@@ -1474,7 +1475,7 @@ for (i=tileSizeMinusOne; i<size; ++i)
 	    tList = gf->lists[bits];
 	    for (j=0; j<listSize; ++j)
 		{
-		gfOffset tStart = tList[j];
+		int tStart = tList[j];
 		if (target == NULL || 
 			(target == findSource(gf, tStart) && tStart >= tMin && tStart < tMax) ) 
 		    {
@@ -1654,7 +1655,7 @@ int size = seq->size;
 int tileSize = gf->tileSize;
 int tileTailSize = gf->segSize;
 int tileHeadSize = gf->tileSize - tileTailSize;
-gfOffset lastStart = size - tileSize;
+int lastStart = size - tileSize;
 char *poly = seq->dna;
 int i, j;
 int tileHead, tileTail;
